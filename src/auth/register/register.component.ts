e22dffb004d96761ac4a2a0cd408117920b1c387
Manakal <manakal@randomsoftware.net>
2023-01-15 16:21:46 +0530

    feat: :sparkles: Added ngRx

--- src/auth/register/register.component.ts
+++ src/auth/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { registerAction } from 'src/store/actions';
 
 @Component({
   selector: 'app-register',
@@ -9,7 +11,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   form!: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private store: Store) {}
   ngOnInit(): void {
     this.initializeForm();
   }
@@ -25,5 +27,6 @@ export class RegisterComponent implements OnInit {
 
   onSubmit(): void {
     console.log('submit', this.form.value, this.form.valid);
+    this.store.dispatch(registerAction(this.form.value));
   }
 }
